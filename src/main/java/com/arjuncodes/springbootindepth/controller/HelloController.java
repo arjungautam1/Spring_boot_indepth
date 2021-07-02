44f9cd2fa0ad96812995e77af09c7351072b10f5
arjungautam1 <arjungautam5431@gmail.com>
2021-07-02 09:58:10 +0545

    adding configuration

--- src/main/java/com/arjuncodes/springbootindepth/controller/HelloController.java
+++ src/main/java/com/arjuncodes/springbootindepth/controller/HelloController.java
@@ -5,6 +5,7 @@
  */
 package com.arjuncodes.springbootindepth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
    /* @RequestMapping(value = "/arjun",method = RequestMethod.GET )*/
     @GetMapping("/arjun")
     public String helloWorld(){
-        return "Welcome to Arjun Codes !! ";
+        return welcomeMessage;
     }
 
 }
